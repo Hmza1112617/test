5e5e473cd5b522c748c751e81104b34a02ff583e
H M Z A <107431235+Hmza1112617@users.noreply.github.com>
2024-11-11 19:32:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,1 @@
-import telebot
-#لا تحاول يا غشاش 🤣
-TOKEN = "token"
-
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start', 'help'])
-def se(message):
-    bot.reply_to(message, "أكوتشي لوتشي موتشي 😂")
-bot.infinity_polling()
+شعدك 🤣🤣
